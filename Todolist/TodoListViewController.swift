f66951473f18235e4cd425675c85e05398f8763f
ASwiftguru <rajinikrai@gmail.com>
2019-07-08 20:45:07 +0200

    able to add items to the UI table

--- Todolist/TodoListViewController.swift
+++ Todolist/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["First array", "second value", "third value"]
+    var itemArray = ["First array", "second value", "third value"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -43,6 +43,33 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - Add new Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+    
+    let alert = UIAlertController(title: "Add a new ToDoList item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add a new item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create a new item"
+            textField = alertTextField
+          
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    
+    
+    }
+    
     
     
 }
